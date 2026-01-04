261617a47b560f059a119d48f2928ad376d59a74
omarley <omarg@live.dk>
2026-01-04 19:50:06 +0100

    test

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,20 @@
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import path from "node:path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  envDir: path.resolve(__dirname, "../server"),
-  resolve: {
-    alias: {
-      "~": path.resolve(__dirname, "src"),
-      "@": path.resolve(__dirname, "../shared"),
+export default defineConfig(({ mode }) => {
+  console.log(path.resolve(__dirname, "../."));
+  console.log("/home/omar/fetchBolig.js");
+
+  return {
+    plugins: [vue(), tailwindcss()],
+    envDir: path.resolve(__dirname, "../."), // <- repo root
+    resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "src"),
+        "@": path.resolve(__dirname, "../shared"),
+      },
     },
-  },
+  };
 });
